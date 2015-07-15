47ad3aa6fd5c27cff4097d515dd113621b36398b
Elmer Ibayan <eibayan@gmail.com>
2015-07-15 21:37:09 +1000

    Managed to get column and row check working.

--- script/app.js
+++ script/app.js
@@ -1,76 +1,140 @@
 var game = {
 
-  board: ['r0c0', 'r0c1', 'r0c2', 'r1c0', 'r1c1', 'r1c2', 'r2c0', 'r2c1', 'r2c2'],
+  //board: ['r0c0', 'r0c1', 'r0c2', 'r1c0', 'r1c1', 'r1c2', 'r2c0', 'r2c1', 'r2c2'],
 
-  boardValues: [null, null, null, null, null, null, null, null, null],
+  boardValues: [[], [], []],
 
-  player1: {score: 0, name: '', avatar: ''},
+  boardSize: 3,
 
-  player2: {score: 0, name: '', avatar: ''},
+  player1: {score: 0, name: '', avatar: '', move: 0},
+
+  player2: {score: 0, name: '', avatar: '', move: 2},
 
   gameScore: 0,
 
-  setRound: 5, 
+  setRound: 5,
 
-  initBoard: function(){
-    _.each(game.boardValues, function(elem){
-        elem = null;
-    });
-  },
-
+  //15Jul15
   setPlayer: function(){
     //player setup: name, avatar, preferences etc.
 
-  },
+  },//end setPlayer
 
-  updateBoard: function(value, row, col){
-    var location = row + col;
-    _.each(game.board, function(elem, index){
-      if (elem === location){
-        game.boardValues[index] = value;
-      }
+  //15Jul15
+  setTile: function (row, col, value){
+    game.boardValues[row][col] = value;
+  }, //end setTile
+
+  //15Jul15
+  initBoard: function(arr){
+    _.each(arr, function(elem, index1){
+        _.each(arr, function(elem, index2){
+          game.setTile(index1, index2, 1);
+        });
     });
+  },//end initBoard
+
+
+  //15Jul15
+  checkLineSum: function(arr){
+    var lineSum = 0;
+    _.each(arr, function(elem){
+       lineSum += elem;   
+    });
+    // console.log("Linesum: "+ lineSum);
+    return lineSum;
   },
 
-  checkWin: function(){
-    var row = game.checkRow();
-    var col = game.checkCol();
-    var cross = game.checkDiagonal();
-    if ((row || col || cross) === true){
+  //15Jul15
+  checkLineMove: function(arr){
+    var sum = game.checkLineSum(arr)
+    if (sum === 0){
+      return true;
+    }else if (sum === 6){
       return true;
     }else{
       return false;
     }
   },
 
-  checkRow: function(arr){
-    //3 conditions
-    var newArray = [];
-    //check top
-    newArray = _.first(arr, 3);
+  //ROW CHECKING:
 
-    //check middle
+  //15Jul15
+  checkRow: function (arr){
+    var rowOut = false;
+    _.each(arr, function(elem){
+      var result = game.checkLineMove(elem);
+        if (result === true){
+          output = true;
+          return rowOut;
+        }
+    });
+    return rowOut;
+  },//end checkRow
+
+  //COLUMN CHECKING:
+
+  //15Jul15
+  checkCol: function(arr){
+    var colArr = [];
+    var columnOut = false;
+
+    // 4 determines size of array
+    for (var i = 0; i <= game.boardSize; i++){
+      
+      //column builder
+      _.each(arr, function(elem1, index){
+        colArr.push(elem1[i]);
+      }); //end of column builder
+
+      columnOut = game.checkLineMove(colArr);
+      if ( columnOut === true) {
+        return columnOut; // declare outside _.each function
+      }else{
+        colArr = []; //reset to get next column
+      }
+
+    } //end of for
+      return columnOut;//default to false!
+  },//end of checkCol
+
+  //nty
+  checkDiag: function(arr){
+    var diagArr = [];
+    var diagOut = false;
+
+    for (var i = 0; i <= game.boardSize; i++){
+
+    //check top-left, bottom-right
+      //column builder
+      _.each(arr, function(elem1, index){
+        diagArr.push(elem1[i]);
+      }); //end of column builder
+
+    //check top-right, bottom-left
+
+    } //end of for
+      //default to false!
+      return diagOut;
+  }, //end of checkDiag
 
 
-    //check bottom
-    return true;
-    return false;
-  },
+//------------------------------------------
 
-  checkCol: function(row, col){
-    //3 conditions
-    return true;
-    return false;
 
-  },
-  checkDiagonal: function(row, col){
-    //2 conditions
-    return true;
-    return false;
+  //nty
+  checkWin: function(){
+    var row = game.checkRow();
+    var col = game.checkCol();
+    var cross = game.checkDiag();
+    if ((row || col || cross) === true){
+      return true;
+    }else{
+      return false;
+    }
+  }, //end checkWin
 
-  },
-
-  
+  //nty
   updateScore: function(player){
     if (player === player1){
       game.player1.score += 1;
@@ -80,6 +144,7 @@ var game = {
 
   },
 
+  //nty
   declareWinner: function(){
     if (game.player1.score === game.setRound){
       return game.player1.name;
@@ -91,8 +156,10 @@ var game = {
   },
 
   init: function(){
-    game.initBoard();
-    game.setPlayer();    
+
+    game.initBoard(game.boardValues); //working
+    game.setPlayer(); //nty
+    console.log("Init Completed!");   
   }
 
 } //end of game
@@ -102,7 +169,19 @@ window.onload = function() {
 
   //var ENTER_KEY = 13; //enter key code
 
+  //15Jul
   game.init();
 
 
 } //end of window onload
+
+
+  // //nty
+  // updateBoard: function(value, row, col){
+  //   var location = row + col;
+  //   _.each(game.board, function(elem, index){
+  //     if (elem === location){
+  //       game.boardValues[index] = value;
+  //     }
+  //   });
+  // },
